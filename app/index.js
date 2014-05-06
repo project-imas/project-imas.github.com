a43fa14aa884a8e5d4c2f84c9549d33582d92402
Lev Novikov <lnovikov@mitre.org>
2014-05-06 10:12:00 -0400

    add: star and fork count to security component header

--- app/index.js
+++ app/index.js
@@ -2,5 +2,20 @@ define(function(require) {
   require('bootstrap');
   require('jquery-gray');
 
-  var $ = require('jquery');
+  var
+    $ = require('jquery'),
+    stars = 0, forks = 0;
+
+  $('#repositories h4[id]').each(function () {
+    var $this = $(this),
+      url = 'https://api.github.com/repos/project-imas/' + $this.attr('id');
+
+    $.getJSON(url).done(function (data) {
+      stars += data.watchers_count;
+      forks += data.forks_count;
+
+      $('.stat.stars').text(stars);
+      $('.stat.forks').text(forks);
+    });
+  });
 });
